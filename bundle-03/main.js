84e0924c14e9f7ba5e540a9473c0eccaaab33a2c
mattiagiaco <martex781227@gmail.com>
2021-12-10 10:44:36 +0100

    Milestone 2 complete

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -56,7 +56,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,14 +64,14 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
 
 const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
+    return auto.type.toLowerCase() === 'diesel';
 });
 
 const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+    return auto.type.toLowerCase() !== 'benzina' && auto.type !== 'diesel';
 });
 
 console.log('Auto a benzina');
@@ -84,3 +84,9 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+// Dato un array di oggetti che contiene diverse auto, si filtrano le macchine in base ad un certo tipo, specificato all'interno dell'oggetto stesso.
+// Manca una virgola dopo la graffa nell'array di oggetti a riga 59
+// A riga 67 non è >= ma =>
+// A riga 70 va aggiunto return
+// Va aggiunto toLowerCase o toUpperCase a riga 67,70 e 74
+// Bisogna sostituire or con and a riga 74
